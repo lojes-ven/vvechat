355de6baac61ffd9159e9ace9c7b45a8aefa7c24
lojes <18914412705@163.com>
2025-12-14 21:15:47 +0800

    feat: new function: IsUidExist and GetUserByUid

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -16,14 +16,40 @@ func NewUserService(db *gorm.DB) *UserService {
 	return &UserService{db}
 }
 
+func (s *UserService) GetUserByUid(uid string) (*model.User, error) {
+	var user model.User
+	res := s.db.Where("uid = ?", uid).First(&user)
+
+	if res.Error != nil {
+		return nil, res.Error
+	}
+
+	return &user, nil
+}
+
+func (s *UserService) IsUidExist(uid string) (bool, error) {
+	var cnt int64
+	res := s.db.Where("id = ?", uid).Count(&cnt)
+	if res.Error != nil {
+		return false, res.Error
+	}
+
+	if cnt > 0 {
+		return true, nil
+	}
+	return false, nil
+}
+
 func (s *UserService) Register(user *model.User) error {
 	if user.Uid == "" || user.Password == "" {
 		return errors.New("密码或微信号不能为空！")
 	}
 
-	var cnt int64
-	s.db.Where("id = ?", user.Uid).Count(&cnt)
-	if cnt > 0 {
+	ok, err := s.IsUidExist(user.Uid)
+	if err != nil {
+		return err
+	}
+	if ok == true {
 		return errors.New("微信号已存在！")
 	}
 
